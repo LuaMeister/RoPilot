d05e331f4ceff2f4107230f06bd82c93c8d354ef
Ryan Hollink <76266509+LuaMeister@users.noreply.github.com>
2022-11-20 21:08:26 +0100

    class controller

---
+++ src/templates/classTemplate.lua
@@ -0,0 +1,35 @@
+
+local ScriptEditorService = game:GetService("ScriptEditorService")
+local Template = {}
+Template.Pattern = "--class%-(%w+)%-"
+Template.Content = [[
+
+local %s = {}
+%s.__index = %s
+
+function %s.new()
+    local new%s = setmetatable(%s, {})
+
+    return new%s
+end
+
+return %s]]
+
+function Template:Match(stringToMatch: string): string?
+    return string.match(stringToMatch, Template.Pattern)
+end
+
+function Template:Execute(document: ScriptDocument, line: string, tag: string?)
+    local content = ""
+
+    if (tag ~= nil) then
+        content = string.format(Template.Content, tag, tag, tag, tag, tag, tag, tag, tag)
+    else
+        content = Template.Content
+    end
+
+    local LineNumber, _, _, _ = document:GetSelection()
+    document:EditTextAsync(content, LineNumber, 1, LineNumber, string.len(line) + 1)
+end
+
+return Template
